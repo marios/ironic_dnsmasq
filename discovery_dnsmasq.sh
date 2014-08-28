895d3033b71fd980550bc9ab7cbbff89dee3921b
marios <marios@redhat.com>
2014-08-28 14:33:59 +0000

    Using 'maintenance' mode to distinguish unkown nodes
    This addresses the artifical scenario specific to VMs. Although we
    have nodes registered in ironic, they aren't 'discovered' yet. Since
    we need to make a blacklist of 'discovered' nodes, we set maintenance
    mode to True on 'unkown' nodes. These are then skipped from the
    blacklist and can then be served by discovery dnsmasq.

--- discovery_dnsmasq.sh
+++ discovery_dnsmasq.sh
@@ -13,38 +13,64 @@ function debug_msg {
     fi
 }
 
+function refresh_node_list() {
+    node_list=`ironic node-list --maintenance False | awk '/ .*-.* /{print $2}' `
+    debug_msg "Refreshed node list: ${node_list[*]}"
+}
+
 function refresh_macs() {
-    new_macs=`ironic port-list | awk '/..:..:../{print $4}'`
+    macs=()
+    for node in $node_list ; do 
+        node_macs=`ironic node-port-list $node | awk '/..:..:..:/{print $4}'`
+        for mac in $node_macs ; do
+            macs+=("$mac")
+        done
+    done
+    debug_msg "Got list of macs ${macs[*]}"
 }
 
 function update_host_file() {
         echo -n "" > $host_file
-        for mac in $new_macs; do
+        for mac in "${macs[@]}"; do
             echo "$mac,ignore" >> $host_file
 	done
+	debug_msg "Updated host_file @ $host_file"
+}
+
+function sighup_dnsmasq {
 	pid=$(cat $pid_file)
-	debug_msg "pid is $pid"
+	debug_msg "Sending sighup to dnsmasq @ $pid"
         kill -1 $pid
 }
 
+#get list of node MACs, write ignore file, start dnsmasq:
+debug_msg "Starting... going to refresh nodes & MACs & write ignore file @ $host_file"
+refresh_node_list
 refresh_macs
-dnsmasq_cmd="dnsmasq  --no-hosts --no-resolv --strict-order --bind-interfaces --interface=$interface --conf-file=$dnsmasq_conf --dhcp-hostsfile=$host_file --leasefile-ro --pid-file=$pid_file"
-debug_msg "executing $dnsmasq_cmd";
-$dnsmasq_cmd 
-debug_msg "dnsmasq_pid @ `cat $pid_file`";
-old_macs=()
-while [ $RUN_DISCOVERY == "True" ] ; do
-    refresh_macs
-    if  [[ "${new_macs[@]}" = "${old_macs[@]}" ]] ; then
-	sleep $poll_interval;
-	source discovery_dnsmasq_conf
-    else
-	debug_msg "Rewriting hosts file, SIGHUP"
+update_host_file
+dnsmasq_cmd="dnsmasq --no-hosts --no-resolv --strict-order --bind-interfaces   
+                --interface=$interface                                         
+                --conf-file=$dnsmasq_conf                                      
+                --dhcp-hostsfile=$host_file --leasefile-ro                     
+                --pid-file=$pid_file"                                          
+                                                                               
+debug_msg "Starting dnsmasq like: $dnsmasq_cmd"
+$dnsmasq_cmd                                                                   
+export old_node_list=()                                    
+while [ $RUN_DISCOVERY == "True" ] ; do                                        
+    refresh_node_list                                                               
+    if [[ "${node_list[@]}" = "${old_node_list[@]}" ]] ; then                  
+        sleep $poll_interval;                                                  
+        source discovery_dnsmasq_conf                                          
+   else                                                                       
+	refresh_macs
         update_host_file
-	old_macs=$new_macs
-   fi
-done
-#cleanup
-pid=$(cat $pid_file)
-debug_msg "Killing dnsmasq @ $pid"
-kill -9 $pid
+	sighup_dnsmasq
+        old_node_list=$node_list                                 
+    fi                                                                         
+done                                                                           
+#cleanup                                                                       
+pid=$(cat $pid_file)                                                           
+debug_msg "Shutdown... kill -9 dnsmasq @ $pid"                                             
+kill -9 $pid                                                                   
+
