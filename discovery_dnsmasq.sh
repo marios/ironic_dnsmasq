d1c96a9393f530141da5d8503b8193a0d373d97b
marios <marios@redhat.com>
2014-09-01 15:13:06 +0300

    Whitespace and nits fix, update conf

--- discovery_dnsmasq.sh
+++ discovery_dnsmasq.sh
@@ -4,11 +4,11 @@
 # loop while true
 # get list of ironic ports. if change, add entry to hosts file, sighup.
 
-source discovery_dnsmasq_conf
+source ironic_dnsmasq_conf
 
 # rudimentary logging/debug. You can change 'echo' to whatever
 function debug_msg {
-    if [ $debug -eq 1 ]; then 
+    if [ $debug -eq 1 ]; then
         echo "DEBUG: An Ironic dnsmasq `date`: $1"
     fi
 }
@@ -20,7 +20,7 @@ function refresh_node_list() {
 
 function refresh_macs() {
     macs=()
-    for node in $node_list ; do 
+    for node in $node_list ; do
         node_macs=`ironic node-port-list $node | awk '/..:..:..:/{print $4}'`
         for mac in $node_macs ; do
             macs+=("$mac")
@@ -30,17 +30,17 @@ function refresh_macs() {
 }
 
 function update_host_file() {
-        echo -n "" > $host_file
-        for mac in "${macs[@]}"; do
-            echo "$mac,ignore" >> $host_file
-	done
-	debug_msg "Updated host_file @ $host_file"
+    echo -n "" > $host_file
+    for mac in "${macs[@]}"; do
+        echo "$mac,ignore" >> $host_file
+    done
+    debug_msg "Updated host_file @ $host_file"
 }
 
 function sighup_dnsmasq {
-	pid=$(cat $pid_file)
-	debug_msg "Sending sighup to dnsmasq @ $pid"
-        kill -1 $pid
+    pid=$(cat $pid_file)
+    debug_msg "Sending sighup to dnsmasq @ $pid"
+    kill -1 $pid
 }
 
 #get list of node MACs, write ignore file, start dnsmasq:
@@ -48,29 +48,29 @@ debug_msg "Starting... going to refresh nodes & MACs & write ignore file @ $host
 refresh_node_list
 refresh_macs
 update_host_file
-dnsmasq_cmd="dnsmasq --no-hosts --no-resolv --strict-order --bind-interfaces   
-                --interface=$interface                                         
-                --conf-file=$dnsmasq_conf                                      
-                --dhcp-hostsfile=$host_file --leasefile-ro                     
-                --pid-file=$pid_file"                                          
-                                                                               
+dnsmasq_cmd="dnsmasq --no-hosts --no-resolv --strict-order --bind-interfaces
+                --interface=$interface
+                --conf-file=$dnsmasq_conf
+                --dhcp-hostsfile=$host_file --leasefile-ro
+                --pid-file=$pid_file"
+
 debug_msg "Starting dnsmasq like: $dnsmasq_cmd"
-$dnsmasq_cmd                                                                   
-export old_node_list=()                                    
-while [ $RUN_DISCOVERY == "True" ] ; do                                        
-    refresh_node_list                                                               
-    if [[ "${node_list[@]}" = "${old_node_list[@]}" ]] ; then                  
-        sleep $poll_interval;                                                  
-        source discovery_dnsmasq_conf                                          
-   else                                                                       
-	refresh_macs
+$dnsmasq_cmd
+export old_node_list=()
+while [ $RUN_DISCOVERY == "True" ] ; do
+    refresh_node_list
+    if [[ "${node_list[@]}" = "${old_node_list[@]}" ]] ; then
+        sleep $poll_interval;
+        source discovery_dnsmasq_conf
+    else
+        refresh_macs
         update_host_file
-	sighup_dnsmasq
-        old_node_list=$node_list                                 
-    fi                                                                         
-done                                                                           
-#cleanup                                                                       
-pid=$(cat $pid_file)                                                           
-debug_msg "Shutdown... kill -9 dnsmasq @ $pid"                                             
-kill -9 $pid                                                                   
+        sighup_dnsmasq
+        old_node_list=$node_list
+    fi
+done
+#cleanup
+pid=$(cat $pid_file)
+debug_msg "Shutdown... kill -9 dnsmasq @ $pid"
+kill -9 $pid
 
